645afd00e61cba3a331e462f96a19dae7e41b8d0
Xenon <gonda.armand@gmail.com>
2020-12-02 17:46:26 +0100

    Added footer.

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Footer extends React.Component {
+    render() {
+    return(
+        <footer id = "footer">&copy; 2020 Gonda Armand</footer>
+        )
+    }
+}
+
+export default Footer;
