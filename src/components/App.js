e9ed7a5acede492ad87d1479fa38712b47cdde70
Xenon <gonda.armand@gmail.com>
2020-12-02 17:44:35 +0100

    Changed filename to css since scss was unnecessary.

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import ChampionData from './championdata';
+import Footer from './footer';
 import Header from './header';
 
 
 function App() {
-	return (
+	return ([
 		<div className = "container">
-			<Header/>
+			<Header />
 			<h1 className = "title non-selectable">League Library</h1>
 			<div className = "champion-container">
-			<ChampionData/>
+			<ChampionData />
 			</div>
+			<Footer />
 		</div>
-	);
+	]);
 }
 
 export default App;
