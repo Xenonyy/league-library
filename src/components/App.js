2f08e386d75f66a683a0d1ada5838ebbd15c756c
Xenon <gonda.armand@gmail.com>
2020-12-23 23:52:11 +0100

    Separated the top part of the webapp into 'TopContainer', so it doesnt cause massive styling issues on mobile if there are no results of the search.

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,14 @@ import ChampionData from './championdata';
 import Footer from './footer';
 import Header from './header';
 import Results from './result';
-import Search from './search';
+import TopContainer from './topcont';
 
 
 function App() {
 	return ([
 		<div className = "container">
 			<Header />
-			<span id = "updated">Last Updated: 2020/12/18</span>
-			<h1 className = "title non-selectable">League Library</h1>
-			<Search />
+			<TopContainer />
 			<div className = "champion-container">
 				<ChampionData />
 				<Results />
---
+++ src/components/topcont.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Search from './search';
+
+class TopContainer extends React.Component {
+    render() {
+    return(
+        <div id = "top-container">
+            <span id = "updated">Last Updated: 2020/12/23</span>
+            <h1 className = "title non-selectable">League Library</h1>
+            <Search />
+        </div>
+        )
+    }
+}
+
+export default TopContainer;
