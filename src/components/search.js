73dd5837428df9d60f2a7452e4a1741b40f869f1
Xenon <gonda.armand@gmail.com>
2020-12-23 23:19:30 +0100

    Found another solution to properly display the 'no results' container. No more issues with mobile devices. Added a maxlength to search.

--- src/components/search.js
+++ src/components/search.js
@@ -3,13 +3,13 @@ import React from 'react';
 class Search extends  React.Component {
 
     champSearch = () => {
-        let input, filter, div, divName, i, txtValue, noResults, champCont;
+        let input, filter, div, divName, i, txtValue, noResults;
             input = document.getElementById('search-input');
             filter = input.value.toUpperCase();
             div = document.getElementsByClassName("champion-card");
             divName = document.getElementsByClassName("champion-card-name");
             noResults = document.getElementById("no-results-container");
-            champCont = document.getElementsByClassName("champion-container")[0];
+            // champCont = document.getElementsByClassName("champion-container")[0];
                 
         // Loop through all list items, and hide those who don't match the search query
         for (i = 0; i < div.length; i++) {
@@ -20,10 +20,24 @@ class Search extends  React.Component {
                 div[i].style.display = "none";
             }
         }
-        if (champCont.clientHeight < 90) {
+        // Convert to an array
+        var divsArray = [].slice.call(div);
+        // Find all divs with display none
+        var displayNone = divsArray.filter(function(el) {
+            return getComputedStyle(el).display === "none"
+        });
+        // And all divs that are not display none
+        var displayShow = divsArray.filter(function(el) {
+            return getComputedStyle(el).display !== "none"
+        });
+        // And use length to count # of divs
+        var numberOfHiddenDivs = displayNone.length;
+        var numberOfVisibleDivs = displayShow.length;
+        console.log("hidden: " + numberOfHiddenDivs + ", visible: " +numberOfVisibleDivs);
+
+        if (numberOfVisibleDivs < 1) {
             noResults.style.display = "flex";
-        }
-        if (champCont.clientHeight > 570) {
+        } else if (numberOfVisibleDivs > 0) {
             noResults.style.display = "none";
         }
     }
@@ -36,6 +50,7 @@ class Search extends  React.Component {
                         id = "search-input"
                         placeholder = "Search..."
                         onChange = {this.champSearch}
+                        maxLength = "15"
                         spellCheck = "false"
                     />
                 </label>
