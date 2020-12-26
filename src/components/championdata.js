ae6f26056513325f58b8e11448807376a8f9717e
Xenon <gonda.armand@gmail.com>
2020-12-26 20:07:50 +0100

    Updated API version. Made code to be a lot  more compact and easily readable.

--- src/components/championdata.js
+++ src/components/championdata.js
@@ -3,6 +3,11 @@ import ChampionCards from "./championmainpage";
 import {ChampionAPI} from "../api/champions";
 import ChampionDetailPanel from './championdetailpanel';
 
+const d = document;
+const version = "10.25.1";
+const apiSpell = "http://ddragon.leagueoflegends.com/cdn/" + version + "/img/spell/";
+const apiPassive = "http://ddragon.leagueoflegends.com/cdn/" + version + "/img/passive/";
+const apiSpellImg = "https://d28xe8vt774jo5.cloudfront.net/champion-abilities/0";
 
 class ChampionData extends React.Component {
     constructor(){
@@ -25,75 +30,94 @@ class ChampionData extends React.Component {
         }
         let cards = this.state.cards;
         let AllData = [];
-        const d = document;
         
         const LoadAllAPI = async function() {
             for (let i = 0; i < cards.length; i++) {
-                const response = await fetch("http://ddragon.leagueoflegends.com/cdn/10.23.1/data/en_US/champion/" + cards[i].id + ".json");
+                const response = await fetch("http://ddragon.leagueoflegends.com/cdn/" + version + "/data/en_US/champion/" + cards[i].id + ".json");
                 const json = await response.json();
                 Object.keys(json.data).forEach(key => {
                     AllData.push(json.data[key]);
                 });
+                const champName = AllData[i].name,
+                    champTitle = AllData[i].title,
+                    champKey = AllData[i].key, 
+                    PName = AllData[i].passive.name,
+                    QName = AllData[i].spells[0].name,
+                    WName = AllData[i].spells[1].name,
+                    EName = AllData[i].spells[2].name,
+                    RName = AllData[i].spells[3].name,
+                    PDesc = AllData[i].passive.description,
+                    QDesc = AllData[i].spells[0].description,
+                    WDesc = AllData[i].spells[1].description,
+                    EDesc = AllData[i].spells[2].description,
+                    RDesc = AllData[i].spells[3].description,
+                    PImg = AllData[i].passive.image.full,
+                    QImg = AllData[i].spells[0].image.full,
+                    WImg = AllData[i].spells[1].image.full,
+                    EImg = AllData[i].spells[2].image.full,
+                    RImg = AllData[i].spells[3].image.full;
+
                 function ChampionKeyVideo(ability) {
-                    if (AllData[i].key.length === 3) {
-                        d.getElementById("champion-detail-abilities-video").src = "https://d28xe8vt774jo5.cloudfront.net/champion-abilities/0" + AllData[i].key + "/ability_0" + AllData[i].key + "_" + ability +"1.webm";
-                        d.getElementById("champion-detail-abilities-video-source-webm").src = "https://d28xe8vt774jo5.cloudfront.net/champion-abilities/0" + AllData[i].key + "/ability_0" + AllData[i].key + "_" + ability +"1.webm";
-                    } else if (AllData[i].key.length === 2) {
-                        d.getElementById("champion-detail-abilities-video").src = "https://d28xe8vt774jo5.cloudfront.net/champion-abilities/00" + AllData[i].key + "/ability_00" + AllData[i].key + "_" + ability +"1.webm";
-                        d.getElementById("champion-detail-abilities-video-source-webm").src = "https://d28xe8vt774jo5.cloudfront.net/champion-abilities/00" + AllData[i].key + "/ability_00" + AllData[i].key + "_" + ability +"1.webm";
-                    } else if (AllData[i].key.length === 1) {
-                        d.getElementById("champion-detail-abilities-video").src = "https://d28xe8vt774jo5.cloudfront.net/champion-abilities/000" + AllData[i].key + "/ability_000" + AllData[i].key + "_" + ability +"1.webm";
-                        d.getElementById("champion-detail-abilities-video-source-webm").src = "https://d28xe8vt774jo5.cloudfront.net/champion-abilities/000" + AllData[i].key + "/ability_000" + AllData[i].key + "_" + ability +"1.webm";
+                    if (champKey.length === 3) {
+                        d.getElementById("champion-detail-abilities-video").src = apiSpellImg + champKey + "/ability_0" + champKey + "_" + ability + "1.webm";
+                        d.getElementById("champion-detail-abilities-video-source-webm").src = apiSpellImg + champKey + "/ability_0" + champKey + "_" + ability + "1.webm";
+                    } else if (champKey.length === 2) {
+                        d.getElementById("champion-detail-abilities-video").src = apiSpellImg + "0" + champKey + "/ability_00" + champKey + "_" + ability + "1.webm";
+                        d.getElementById("champion-detail-abilities-video-source-webm").src = apiSpellImg + "0" + champKey + "/ability_00" + champKey + "_" + ability + "1.webm";
+                    } else if (champKey.length === 1) {
+                        d.getElementById("champion-detail-abilities-video").src = apiSpellImg + "00" + champKey + "/ability_000" + champKey + "_" + ability + "1.webm";
+                        d.getElementById("champion-detail-abilities-video-source-webm").src = apiSpellImg + "00" + champKey + "/ability_000" + champKey + "_" + ability + "1.webm";
                     }
                 }
-                d.getElementById("champion-card-" + i).addEventListener('click',function (){
-                    d.getElementsByTagName("title")[0].innerText = AllData[i].name + ' - ' + AllData[i].title;
+
+                d.getElementById("champion-card-" + i).addEventListener('click', function (){
+                    d.getElementsByTagName("title")[0].innerText = champName + ' - ' + champTitle;
                     d.getElementById("champBgImg").src = 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/' + AllData[i].id + '_0.jpg';
                     d.getElementById("champImg").src = 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/' + AllData[i].id + '_0.jpg';
-                    d.getElementById("champion-detail-name").innerHTML = AllData[i].name;
-                    d.getElementById("champion-detail-title").innerText = AllData[i].title.capitalize();
+                    d.getElementById("champion-detail-name").innerHTML = champName;
+                    d.getElementById("champion-detail-title").innerText = champTitle.capitalize();
                     d.getElementById("champion-detail-description").innerText = AllData[i].blurb;
                     d.getElementById("champion-detail-description-full").innerText = AllData[i].lore;
-                    d.getElementById("champion-detail-abilities-p-name").innerText = AllData[i].passive.name;
-                    d.getElementById("champion-detail-abilities-p").src = "http://ddragon.leagueoflegends.com/cdn/10.23.1/img/passive/" + AllData[i].passive.image.full;
-                    d.getElementById("champion-detail-abilities-q-name").innerText = AllData[i].spells[0].name;
-                    d.getElementById("champion-detail-abilities-q").src = "http://ddragon.leagueoflegends.com/cdn/10.23.1/img/spell/" + AllData[i].spells[0].image.full;
-                    d.getElementById("champion-detail-abilities-w-name").innerText = AllData[i].spells[1].name;
-                    d.getElementById("champion-detail-abilities-w").src = "http://ddragon.leagueoflegends.com/cdn/10.23.1/img/spell/" + AllData[i].spells[1].image.full;
-                    d.getElementById("champion-detail-abilities-e-name").innerText = AllData[i].spells[2].name;
-                    d.getElementById("champion-detail-abilities-e").src = "http://ddragon.leagueoflegends.com/cdn/10.23.1/img/spell/" + AllData[i].spells[2].image.full;
-                    d.getElementById("champion-detail-abilities-r-name").innerText = AllData[i].spells[3].name;
-                    d.getElementById("champion-detail-abilities-r").src = "http://ddragon.leagueoflegends.com/cdn/10.23.1/img/spell/" + AllData[i].spells[3].image.full;
+                    d.getElementById("champion-detail-abilities-p-name").innerText = PName;
+                    d.getElementById("champion-detail-abilities-p").src = apiPassive + PImg;
+                    d.getElementById("champion-detail-abilities-q-name").innerText = QName;
+                    d.getElementById("champion-detail-abilities-q").src = apiSpell + QImg;
+                    d.getElementById("champion-detail-abilities-w-name").innerText = WName;
+                    d.getElementById("champion-detail-abilities-w").src = apiSpell + WImg;
+                    d.getElementById("champion-detail-abilities-e-name").innerText = EName;
+                    d.getElementById("champion-detail-abilities-e").src = apiSpell + EImg;
+                    d.getElementById("champion-detail-abilities-r-name").innerText = RName;
+                    d.getElementById("champion-detail-abilities-r").src = apiSpell + RImg;
 
                     d.getElementById("champion-detail-abilities-p-container").addEventListener('mouseenter', function() {
                         d.getElementById("champion-detail-abilities-extra-detail-container").style.display = "flex";
-                        d.getElementById("champion-detail-abilities-description").innerHTML = AllData[i].passive.description;
+                        d.getElementById("champion-detail-abilities-description").innerHTML = PDesc;
                         ChampionKeyVideo("P");
-                        d.getElementById("champion-detail-ability-name-phone").innerText = "P - " + AllData[i].passive.name;
+                        d.getElementById("champion-detail-ability-name-phone").innerText = "P - " + PName;
                     })
                     d.getElementById("champion-detail-abilities-q-container").addEventListener('mouseenter', function() {
                         d.getElementById("champion-detail-abilities-extra-detail-container").style.display = "flex";
-                        d.getElementById("champion-detail-abilities-description").innerHTML = AllData[i].spells[0].description;
+                        d.getElementById("champion-detail-abilities-description").innerHTML = QDesc;
                         ChampionKeyVideo("Q");
-                        d.getElementById("champion-detail-ability-name-phone").innerText = "Q - " + AllData[i].spells[0].name;
+                        d.getElementById("champion-detail-ability-name-phone").innerText = "Q - " + QName;
                     })
                     d.getElementById("champion-detail-abilities-w-container").addEventListener('mouseenter', function() {
                         d.getElementById("champion-detail-abilities-extra-detail-container").style.display = "flex";
-                        d.getElementById("champion-detail-abilities-description").innerHTML = AllData[i].spells[1].description;
+                        d.getElementById("champion-detail-abilities-description").innerHTML = WDesc;
                         ChampionKeyVideo("W");
-                        d.getElementById("champion-detail-ability-name-phone").innerText = "W - " + AllData[i].spells[1].name;
+                        d.getElementById("champion-detail-ability-name-phone").innerText = "W - " + WName;
                     })
                     d.getElementById("champion-detail-abilities-e-container").addEventListener('mouseenter', function() {
                         d.getElementById("champion-detail-abilities-extra-detail-container").style.display = "flex";
-                        d.getElementById("champion-detail-abilities-description").innerHTML = AllData[i].spells[2].description;
+                        d.getElementById("champion-detail-abilities-description").innerHTML = EDesc;
                         ChampionKeyVideo("E");
-                        d.getElementById("champion-detail-ability-name-phone").innerText = "E - " + AllData[i].spells[2].name;
+                        d.getElementById("champion-detail-ability-name-phone").innerText = "E - " + EName;
                     })
                     d.getElementById("champion-detail-abilities-r-container").addEventListener('mouseenter', function() {
                         d.getElementById("champion-detail-abilities-extra-detail-container").style.display = "flex";
-                        d.getElementById("champion-detail-abilities-description").innerHTML = AllData[i].spells[3].description;
+                        d.getElementById("champion-detail-abilities-description").innerHTML = RDesc;
                         ChampionKeyVideo("R");
-                        d.getElementById("champion-detail-ability-name-phone").innerText = "R - " + AllData[i].spells[3].name;
+                        d.getElementById("champion-detail-ability-name-phone").innerText = "R - " + RName;
                     })
                     d.getElementById("champion-detail-abilities-video").addEventListener('mouseenter', function() {
                         this.muted = false;
@@ -104,7 +128,6 @@ class ChampionData extends React.Component {
                     
                 });
             }
-            // console.log(AllData[2].key[0]);
         }
         LoadAllAPI();
         return([
