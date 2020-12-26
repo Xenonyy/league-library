b2695afa877950a126606c3aeb08fa2493ae0aa0
Xenon <gonda.armand@gmail.com>
2020-12-26 19:07:09 +0100

    Added a 'search reset' button.

--- src/components/search.js
+++ src/components/search.js
@@ -3,19 +3,20 @@ import React from 'react';
 class Search extends  React.Component {
 
     champSearch = () => {
-        let input, filter, div, divName, i, txtValue, noResults;
+        let input, filter, div, divName, i, txtValue, noResults, reset;
             input = document.getElementById('search-input');
             filter = input.value.toUpperCase();
             div = document.getElementsByClassName("champion-card");
             divName = document.getElementsByClassName("champion-card-name");
             noResults = document.getElementById("no-results-container");
             // champCont = document.getElementsByClassName("champion-container")[0];
+            reset = document.getElementById("search-reset");
                 
         // Loop through all list items, and hide those who don't match the search query
         for (i = 0; i < div.length; i++) {
             txtValue = divName[i].innerText;
             if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                div[i].style.display = "";
+                div[i].style.display = "flex";
             } else {
                 div[i].style.display = "none";
             }
@@ -40,6 +41,15 @@ class Search extends  React.Component {
         } else if (numberOfVisibleDivs > 0) {
             noResults.style.display = "none";
         }
+
+        reset.addEventListener("click", () => {
+            input.value = "";
+            for (let i = 0; i < div.length; i++) {
+                div[i].style.display = "flex";
+                
+            }
+        })
+
     }
 	render() {
 		return (
@@ -54,6 +64,8 @@ class Search extends  React.Component {
                         spellCheck = "false"
                     />
                 </label>
+                <div id = "search-reset">X</div>
+
             </div>
         )
 	}
