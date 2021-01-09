08b43e955aa460e90d86d66fe0cc7a2bbd0dfe28
Xenon <gonda.armand@gmail.com>
2021-01-09 22:47:09 +0100

    Updated + reduced code.

--- src/components/backtotop.js
+++ src/components/backtotop.js
@@ -2,19 +2,14 @@ import React from 'react';
 
 class BackToTop extends React.Component {
     render() {
-        window.onload = () => {
-            let b2t = document.getElementById("b2t");
-            window.onscroll = () => {scrollFunc();}
-            function scrollFunc() {
-                if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-                    b2t.style.display = "block";
-                } else {
-                    b2t.style.display = "none";
-                }
+        window.onscroll = () => {
+            let b2t = document.querySelector("#b2t");
+            if (document.documentElement.scrollTop > 300) {
+                b2t.style.display = "block";
+            } else {
+                b2t.style.display = "none";
             }
-
             b2t.addEventListener("click", () => {
-                document.body.scrollTop = 0;
                 document.documentElement.scrollTop = 0;
             })
         }
