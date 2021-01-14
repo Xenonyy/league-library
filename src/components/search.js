90aa196817199f26518270b64bd009c55092dd03
Xenon <gonda.armand@gmail.com>
2021-01-14 15:12:13 +0100

    Updated code(ES6 features). Changed to shorthand syntax. Removed unused code.

--- src/components/search.js
+++ src/components/search.js
@@ -1,55 +1,36 @@
 import React from 'react';
 
 class Search extends  React.Component {
-
     champSearch = () => {
-        let input, filter, div, divName, i, txtValue, noResults, reset;
+        let input, filter, div, divName, noResults, reset;
             input = document.getElementById('search-input');
             filter = input.value.toUpperCase();
             div = document.getElementsByClassName("champion-card");
             divName = document.getElementsByClassName("champion-card-name");
             noResults = document.getElementById("no-results-container");
-            // champCont = document.getElementsByClassName("champion-container")[0];
             reset = document.getElementById("search-reset");
                 
         // Loop through all list items, and hide those who don't match the search query
-        for (i = 0; i < div.length; i++) {
-            txtValue = divName[i].innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                div[i].style.display = "flex";
-            } else {
-                div[i].style.display = "none";
-            }
+        for (const [i] of Object.entries(div)) {
+            divName[i].innerText.toUpperCase().indexOf(filter) > -1 ? div[i].style.display = "flex" : div[i].style.display = "none";
         }
         // Convert to an array
-        var divsArray = [].slice.call(div);
-        // Find all divs with display none
-        // var displayNone = divsArray.filter(function(e) {
-        //     return getComputedStyle(e).display === "none"
-        // });
-        // And all divs that are not display none
-        var displayShow = divsArray.filter(function(e) {
-            return getComputedStyle(e).display !== "none"
-        });
+        const divsArray = [].slice.call(div);
+        // Find all divs that are not display none
+        const displayShow = divsArray.filter((e) => getComputedStyle(e).display !== "none");
         // And use length to count # of divs
-        // var numberOfHiddenDivs = displayNone.length;
-        var numberOfVisibleDivs = displayShow.length;
-        // console.log("hidden: " + numberOfHiddenDivs + ", visible: " +numberOfVisibleDivs);
+        const numberOfVisibleDivs = displayShow.length;
 
-        if (numberOfVisibleDivs < 1) {
-            noResults.style.display = "flex";
-        } else if (numberOfVisibleDivs > 0) {
-            noResults.style.display = "none";
-        }
-
+        if (numberOfVisibleDivs < 1) noResults.style.display = "flex";
+        else if (numberOfVisibleDivs > 0) noResults.style.display = "none";
+        
+        // Reset search on click
         reset.addEventListener("click", () => {
             input.value = "";
-            for (let i = 0; i < div.length; i++) {
-                div[i].style.display = "flex";
-                
+            for (const item of div) {
+                div[item].style.display = "flex";
             }
         })
-
     }
 	render() {
 		return (
@@ -65,7 +46,6 @@ class Search extends  React.Component {
                     />
                 </label>
                 <div id = "search-reset">X</div>
-
             </div>
         )
 	}
