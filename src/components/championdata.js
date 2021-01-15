fa9a6d9b365f5a040ef0fb643735fa12b0f1882a
Xenon <gonda.armand@gmail.com>
2021-01-15 21:43:01 +0100

    Added destructurization. Removed manual toggle for skins for now because of React caching bug.

--- src/components/championdata.js
+++ src/components/championdata.js
@@ -12,8 +12,8 @@ const d = document,
     apiSplash = `${cdn}img/champion/splash/`;
 
 class ChampionData extends React.Component {
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
         this.state = {cards: []}
     }
     async componentDidMount() {
@@ -41,112 +41,87 @@ class ChampionData extends React.Component {
                     AllData.push(json.data[key]);
                 });
 
-                const champ = {};
-                champ.name = AllData[x].name;
-                champ.id = AllData[x].id;
-                champ.title = AllData[x].title;
-                champ.key = AllData[x].key;
-                champ.skin = AllData[x].skins;
-                champ.blurb = AllData[x].blurb;
-                champ.lore = AllData[x].lore;
+                const champion = {
+                    name: AllData[x].name,
+                    id: AllData[x].id,
+                    title: AllData[x].title,
+                    key: AllData[x].key,
+                    skins: AllData[x].skins,
+                    blurb: AllData[x].blurb,
+                    lore: AllData[x].lore,
+                    spell: AllData[x].spells,
+                };
+                const { name, id, title, key, skins, blurb, lore, spell } = champion;
 
-                const spells = {};
-                spells.p = AllData[x].passive;
-                spells.p.name = AllData[x].passive.name;
-                spells.p.desc = AllData[x].passive.description;
-                spells.p.img = AllData[x].passive.image.full;
+                const spells = {
+                    p: {
+                        name: AllData[x].passive.name,
+                        desc: AllData[x].passive.description,
+                        img: AllData[x].passive.image.full,
+                    }
+                };
 
                 for (const [num, ability] of Object.entries(['q', 'w', 'e', 'r'])) {
-                    spells[ability] = AllData[x].spells[num];
-                    spells[ability].name = AllData[x].spells[num].name;
-                    spells[ability].desc = AllData[x].spells[num].description;
-                    spells[ability].img = AllData[x].spells[num].image.full;
+                    spells[ability] = spell[num];
+                    spells[ability].name = spell[num].name;
+                    spells[ability].desc = spell[num].description;
+                    spells[ability].img = spell[num].image.full;
                 }
-            
                 function ChampionKeyVideo(ability) {
-                    if (champ.key.length === 3) {
-                        d.getElementById("champion-detail-abilities-video").src = `${apiSpellImg + champ.key}/ability_0${champ.key}_${ability}1.webm`;
-                        d.getElementById("champion-detail-abilities-video-source-webm").src = `${apiSpellImg + champ.key}/ability_0${champ.key}_${ability}1.webm`;
-                    } else if (champ.key.length === 2) {
-                        d.getElementById("champion-detail-abilities-video").src = `${apiSpellImg}0${champ.key}/ability_00${champ.key}_${ability}1.webm`;
-                        d.getElementById("champion-detail-abilities-video-source-webm").src = `${apiSpellImg}0${champ.key}/ability_00${champ.key}_${ability}1.webm`;
-                    } else if (champ.key.length === 1) {
-                        d.getElementById("champion-detail-abilities-video").src = `${apiSpellImg}00${champ.key}/ability_000${champ.key}_${ability}1.webm`;
-                        d.getElementById("champion-detail-abilities-video-source-webm").src = `${apiSpellImg}00${champ.key}/ability_000${champ.key}_${ability}1.webm`;
+                    if (key.length === 3) {
+                        d.getElementById("champion-detail-abilities-video").src = `${apiSpellImg + key}/ability_0${key}_${ability}1.webm`;
+                        d.getElementById("champion-detail-abilities-video-source-webm").src = `${apiSpellImg + key}/ability_0${key}_${ability}1.webm`;
+                    } else if (key.length === 2) {
+                        d.getElementById("champion-detail-abilities-video").src = `${apiSpellImg}0${key}/ability_00${key}_${ability}1.webm`;
+                        d.getElementById("champion-detail-abilities-video-source-webm").src = `${apiSpellImg}0${key}/ability_00${key}_${ability}1.webm`;
+                    } else if (key.length === 1) {
+                        d.getElementById("champion-detail-abilities-video").src = `${apiSpellImg}00${key}/ability_000${key}_${ability}1.webm`;
+                        d.getElementById("champion-detail-abilities-video-source-webm").src = `${apiSpellImg}00${key}/ability_000${key}_${ability}1.webm`;
                     }
                 }
                 d.querySelector(`#champion-card-${x}`).addEventListener('click', () => {
-                    d.getElementsByTagName("title")[0].innerText = `${champ.name} - ${champ.title}`;
-                    d.getElementById("champBgImg").src = `${apiSplash + champ.id}_0.jpg`;
-                    d.getElementById("champImg").src = `${apiSplash + champ.id}_0.jpg`;
-                    d.getElementById("champion-detail-name").innerText = champ.name;
-                    d.getElementById("champion-detail-title").innerText = champ.title.capitalize();
-                    d.getElementById("champion-detail-description").innerText = champ.blurb;
-                    d.getElementById("champion-detail-description-full").innerText = champ.lore;
+                    console.log(...name)
+                    d.getElementsByTagName("title")[0].innerText = `${name} - ${title}`;
+                    d.getElementById("champBgImg").src = `${apiSplash + id}_0.jpg`;
+                    d.getElementById("champImg").src = `${apiSplash + id}_0.jpg`;
+                    d.getElementById("champion-detail-name").innerText = name;
+                    d.getElementById("champion-detail-title").innerText = title.capitalize();
+                    d.getElementById("champion-detail-description").innerText = blurb;
+                    d.getElementById("champion-detail-description-full").innerText = lore;
                     for (const [key, spell] of Object.entries(spells)) {
                         d.querySelector(`#champion-detail-abilities-${key}-name`).innerText = spell.name;
                         d.querySelector(`#champion-detail-abilities-${key}`).src = apiSpell + spell.img;
                     }
                     d.getElementById("champion-detail-abilities-p").src = apiPassive + spells.p.img;
-                    skinDiv.src = `${apiSplash + champ.id}_0.jpg`;
-                    skinBgDiv.src = `${apiSplash + champ.id}_0.jpg`;
-                    d.getElementById("champion-detail-skins-name").innerText = champ.name;
-
-                    // Proof of bug
-                    console.log(champ.name)
-
+                    skinDiv.src = `${apiSplash + id}_0.jpg`;
+                    skinBgDiv.src = `${apiSplash + id}_0.jpg`;
+                    d.getElementById("champion-detail-skins-name").innerText = name;
 
                     // Slideshow of skins
                     const timer = ms => new Promise(res => setTimeout(res, ms)) // Returns a Promise that resolves after "ms" Milliseconds
                     let abort = false;
                     let j = 0;
-                    // Manual toggle
-                    d.getElementsByClassName("prev")[0].addEventListener("click", () => {
-                        abort = true;
-                        console.log(j, champ.skin.length)
-                        if (j <= 0) {
-                            return;
-                        } else {
-                            skinDiv.classList.remove("hide");
-                            skinBgDiv.classList.remove("hide");
-                            skinDiv.src = `${apiSplash + champ.id}_${champ.skin[j - 1].num}.jpg`;
-                            skinBgDiv.src = `${apiSplash + champ.id}_${champ.skin[j - 1].num}.jpg`;
-                            d.getElementById("champion-detail-skins-name").innerText = champ.skin[j - 1].name;
-                            if (champ.skin[j - 1].name === "default") d.getElementById("champion-detail-skins-name").innerText = champ.name;
-                            j--;
-                        }
-                    })
-                    d.getElementsByClassName("next")[0].addEventListener("click", () => {
-                        abort = true;
-                        console.log(j, champ.skin.length)
-                        if (j + 1 >= champ.skin.length) {
-                            return;
-                        } else {
-                            skinDiv.classList.remove("hide");
-                            skinBgDiv.classList.remove("hide");
-                            skinDiv.src = `${apiSplash + champ.id}_${champ.skin[j + 1].num}.jpg`;
-                            skinBgDiv.src = `${apiSplash + champ.id}_${champ.skin[j + 1].num}.jpg`;
-                            d.getElementById("champion-detail-skins-name").innerText = champ.skin[j + 1].name;
-                            if (champ.skin[j + 1].name === "default") d.getElementById("champion-detail-skins-name").innerText = champ.name;
-                            j++;
-                        }
-                    })
+                    const showDivs = () => {
+                        skinDiv.classList.remove("hide");
+                        skinBgDiv.classList.remove("hide-almost");
+                    }
+                    
                     // Automatic slideshow
                     async function slideShow () {
-                        for (j = 0; j < champ.skin.length; j++) {
+                        for (j = 0; j < skins.length; j++) {
+                            console.log(j)
                             if (abort === false) {
-                                skinDiv.src = `${apiSplash + champ.id}_${champ.skin[j].num}.jpg`;
-                                skinBgDiv.src = `${apiSplash + champ.id}_${champ.skin[j].num}.jpg`;
-                                skinDiv.classList.remove("hide");
-                                skinBgDiv.classList.remove("hide");
-                                if (champ.skin[j].name === "default") d.getElementById("champion-detail-skins-name").innerText = champ.name;
-                                else d.getElementById("champion-detail-skins-name").innerText = champ.skin[j].name;
+                                skinDiv.src = `${apiSplash + id}_${skins[j].num}.jpg`;
+                                skinBgDiv.src = `${apiSplash + id}_${skins[j].num}.jpg`;
+                                // await timer(650); // Wait for the skins to load before showing
+                                showDivs();
+                                if (skins[j].name === "default") d.getElementById("champion-detail-skins-name").innerText = name;
+                                else d.getElementById("champion-detail-skins-name").innerText = skins[j].name;
 
                                 // Change the skin every 3 seconds with fade out effect
                                 await timer(3000);
                                     if (abort === true) {
-                                        skinDiv.classList.remove("hide");
-                                        skinBgDiv.classList.remove("hide");
+                                        showDivs();
                                         return;
                                     } else {
                                         skinDiv.classList.add("hide");
@@ -154,27 +129,57 @@ class ChampionData extends React.Component {
                                         await timer(500);
                                     }
                             }
+                            if (j  + 1 >= skins.length) j = -1; // Reset slideshow if it reached the end
                         }
                         // Reset to default if reached the end
-                        if (abort === false) {
-                            if (j >= champ.skin.length) {
-                                skinDiv.classList.remove("hide");
-                                skinBgDiv.classList.remove("hide");
-                                skinDiv.src = `${apiSplash + champ.id}_0.jpg`;
-                                skinBgDiv.src = `${apiSplash + champ.id}_0.jpg`;
-                                d.getElementById("champion-detail-skins-name").innerText = champ.name;
-                            }
-                        }
+                        // if (abort === false) {
+                        //     if (j >= skins.length) {
+                        //         showDivs();
+                        //         skinDiv.src = `${apiSplash + id}_0.jpg`;
+                        //         skinBgDiv.src = `${apiSplash + id}_0.jpg`;
+                        //         d.getElementById("champion-detail-skins-name").innerText = name;
+                        //         await timer(200);
+                        //         d.querySelector("#slideShowEnd").classList.remove("hide");
+                        //     }
+                        // }
+                        // Manual toggle
+                        // d.getElementsByClassName("prev")[0].addEventListener("click", () => {
+                        //     abort = true;
+                        //     console.log(j, skins.length)
+                        //     if (j <= 0) {
+                        //         return;
+                        //     } else {
+                        //         showDivs();
+                        //         skinDiv.src = `${apiSplash + id}_${skins[j - 1].num}.jpg`;
+                        //         skinBgDiv.src = `${apiSplash + id}_${skins[j - 1].num}.jpg`;
+                        //         d.getElementById("champion-detail-skins-name").innerText = skins[j - 1].name;
+                        //         if (skins[j - 1].name === "default") d.getElementById("champion-detail-skins-name").innerText = name;
+                        //         j--;
+                        //     }
+                        // })
+                        // d.getElementsByClassName("next")[0].addEventListener("click", () => {
+                        //     abort = true;
+                        //     console.log(j, skins.length)
+                        //     if (j + 1 >= skins.length) {
+                        //         return;
+                        //     } else {
+                        //         showDivs();
+                        //         skinDiv.src = `${apiSplash + id}_${skins[j + 1].num}.jpg`;
+                        //         skinBgDiv.src = `${apiSplash + id}_${skins[j + 1].num}.jpg`;
+                        //         d.getElementById("champion-detail-skins-name").innerText = skins[j + 1].name;
+                        //         if (skins[j + 1].name === "default") d.getElementById("champion-detail-skins-name").innerText = name;
+                        //         j++;
+                        //     }
+                        // })
                     }
                     slideShow();
                     d.getElementsByClassName("close-button")[0].addEventListener('click', () => {
                         abort = true;
                         j = 0;
                         // Proof of bug
-                        console.log(champ.name)
+                        console.log(name)
                     })
-
-                    // Show information when hovering
+                    // Show abilities information when hovering
                     for (const [x, ability] of Object.entries(spells)) {
                         d.querySelector(`#champion-detail-abilities-${x}-container`).addEventListener('mouseenter', function () {
                             d.getElementById("champion-detail-abilities-extra-detail-container").style.display = "flex";
@@ -183,12 +188,8 @@ class ChampionData extends React.Component {
                             ChampionKeyVideo(`${x.toUpperCase()}`);
                         });
                     }
-                    d.getElementById("champion-detail-abilities-video").addEventListener('mouseenter', function () {
-                        this.muted = false;
-                    })
-                    d.getElementById("champion-detail-abilities-video").addEventListener('mouseleave', function () {
-                        this.muted = true;
-                    })
+                    d.getElementById("champion-detail-abilities-video").addEventListener('mouseenter', function () { this.muted = false })
+                    d.getElementById("champion-detail-abilities-video").addEventListener('mouseleave', function () { this.muted = true })
 
                     // Lower font size if line break would occur
                     for (const div of d.querySelectorAll(".abilities-name")) {
@@ -196,6 +197,7 @@ class ChampionData extends React.Component {
                         if (div.clientHeight <= 32) continue;
                             div.style.fontSize = "0.8rem";
                             div.style.whiteSpace = "nowrap";
+                            if (d.body.clientWidth < 1400) div.style.fontSize = "0.7rem";
                     }
                 });
             }
