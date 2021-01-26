11b7d75b75c800ced8024b97a00b3588a68337f5
Xenon <gonda.armand@gmail.com>
2021-01-27 00:20:44 +0100

    Updated date.

--- src/components/search/TopContainer.jsx
+++ src/components/search/TopContainer.jsx
@@ -5,7 +5,7 @@ class TopContainer extends React.Component {
     render() {
     return(
         <div id = "top-container">
-            <span id = "updated">Last Updated: 2021/01/16</span>
+            <span id = "updated">Last Updated: 2021/01/27</span>
             <h1 className = "title non-selectable">League Library</h1>
             <Search />
         </div>
