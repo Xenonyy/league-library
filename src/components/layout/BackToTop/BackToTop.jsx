925f10999b88a912e9b63ffaa7d1cd03236121fa
Xenon <gonda.armand@gmail.com>
2021-02-18 23:12:46 +0100

    Fixed bug that caused glitching if the second image wasnt already loaded.

--- src/components/layout/BackToTop/BackToTop.jsx
+++ src/components/layout/BackToTop/BackToTop.jsx
@@ -15,7 +15,7 @@ class BackToTop extends React.Component {
         }
     return(
         <button id = "b2t" >
-            <img src = "https://i.gyazo.com/8fde68144b0fe90786b9472cdcad77f1.png" alt = "Scroll To The Top" id = "b2t-img"></img>
+            <img src = "https://i.gyazo.com/8fde68144b0fe90786b9472cdcad77f1.png" srcSet = "https://i.gyazo.com/8fde68144b0fe90786b9472cdcad77f1.png, https://i.gyazo.com/25bca84d684ddfe5d7dd4aaec54f92e6.png" alt = "Scroll To The Top" id = "b2t-img"></img>
         </button>
         )
     }
