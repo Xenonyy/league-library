a1da64c2d7690dd0834033da95eca01897fe5dd6
Xenon <gonda.armand@gmail.com>
2021-04-15 21:30:19 +0200

    Fixed a bug where on desktop the button would completely not work. Updated syntax to ES6.

--- src/components/layout/BackToTop/BackToTop.jsx
+++ src/components/layout/BackToTop/BackToTop.jsx
@@ -9,18 +9,21 @@ class BackToTop extends React.Component {
             } else {
                 b2t.style.display = "none";
             }
+            b2t.addEventListener("click", () => {
+                document.documentElement.scrollTop = 0;
+            });
         }
         window.onload = () => {
-            document.querySelector("button > img").addEventListener("click", function(e) {
+            document.querySelector("button > img").addEventListener("click", (e) => {
                 e.stopPropagation();
                 e.preventDefault();
             });
-            document.querySelector("#b2t").addEventListener("click", function() {
+            document.querySelector("#b2t").addEventListener("click", () => {
                 document.documentElement.scrollTop = 0;
             });
         }
     return(
-        <button id = "b2t" >
+        <button id = "b2t">
             <img src = "https://i.gyazo.com/8fde68144b0fe90786b9472cdcad77f1.png" srcSet = "https://i.gyazo.com/8fde68144b0fe90786b9472cdcad77f1.png, https://i.gyazo.com/25bca84d684ddfe5d7dd4aaec54f92e6.png" alt = "Scroll To The Top" id = "b2t-img"></img>
         </button>
         )
