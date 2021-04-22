f7d7c2defbca63a104e1a8c9aa314d12957c4d7d
Xenon <gonda.armand@gmail.com>
2021-04-22 17:23:49 +0200

    Refactored for test.

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 
 class Search extends  React.Component {
     champSearch = () => {
-        let input, filter, div, divName, noResults, reset;
+        let input, filter, div, divName, noResults;
             input = document.getElementById('search-input');
             filter = input.value.toUpperCase();
             div = document.getElementsByClassName("champion-card");
             divName = document.getElementsByClassName("champion-card-name");
             noResults = document.getElementById("no-results-container");
-            reset = document.getElementById("search-reset");
                 
         // Loop through all list items, and hide those who don't match the search query
         for (const [i] of Object.entries(div)) {
@@ -23,18 +22,19 @@ class Search extends  React.Component {
 
         if (numberOfVisibleDivs < 1) noResults.style.display = "flex";
         else if (numberOfVisibleDivs > 0) noResults.style.display = "none";
+    }
+     // Reset search on click
+     resetSearch = () => {
+        document.querySelector('#search-input').value = "";
         
-        // Reset search on click
-        reset.addEventListener("click", () => {
-            input.value = "";
-            for (const [index] of Object.entries(div)) {
-                div[index].style.display = "flex";
-            }
-        })
+        const div = document.querySelectorAll(".champion-card");
+        for (const [index] of Object.entries(div)) {
+            div[index].style.display = "flex";
+        }
     }
 	render() {
 		return (
-            <div className = "search-container">
+            <div className = "search-container" data-testid = "search-container">
                 <label className = "search-label" htmlFor = "search-input">
                     <input
                         type = "text"
@@ -43,9 +43,10 @@ class Search extends  React.Component {
                         onChange = {this.champSearch}
                         maxLength = "15"
                         spellCheck = "false"
+                        data-testid = "search-input"
                     />
                 </label>
-                <div id = "search-reset">X</div>
+                <div id = "search-reset" data-testid = "reset" onClick = {() => this.resetSearch()}>X</div>
             </div>
         )
 	}
