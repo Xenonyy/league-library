83bad98d732382612a99cb41d03eebc9241261bd
Xenon <gonda.armand@gmail.com>
2022-01-23 18:25:45 +0100

    feat: add utils

---
+++ src/utils/setVolume.ts
@@ -0,0 +1,6 @@
+export const setVolume = () => {
+    const video = document.getElementById("champion-detail-abilities-video") as HTMLVideoElement;
+    video.volume = 0.3;
+    video.addEventListener('mouseenter', function () { this.muted = false });
+    video.addEventListener('mouseleave', function () { this.muted = true });
+}
