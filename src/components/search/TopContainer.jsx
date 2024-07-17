d324aa1459b00c0dbb80f4031a3e7213a21056ea
Xenonyy <gonda.armand@gmail.com>
2024-07-17 07:04:20 +0200

    style: formatting

--- src/components/search/TopContainer.jsx
+++ src/components/search/TopContainer.jsx
@@ -3,13 +3,15 @@ import { Version } from '../../enums/version';
 import Search from './Search';
 
 const TopContainer = () => {
-    return(
-        <div id = "top-container">
-            <span id = "updated">{`Last Updated: 2022/01/23 | Patch: ${Version.PATCH}`}</span>
-            <h1 data-testid = "league-title" className = "title non-selectable">League Library</h1>
-            <Search />
-        </div>
-    )
-}
+  return (
+    <div id="top-container">
+      <span id="updated">{`Last Updated: 2022/01/23 | Patch: ${Version.PATCH}`}</span>
+      <h1 data-testid="league-title" className="title non-selectable">
+        League Library
+      </h1>
+      <Search />
+    </div>
+  );
+};
 
 export default TopContainer;
