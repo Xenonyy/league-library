8229f61395fcbd367187c06ca5e466cb14936fcc
Xenonyy <gonda.armand@gmail.com>
2024-07-17 06:33:26 +0200

    fix/refactor: corrected links, link behaviours and functionality

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -12,36 +12,50 @@ class Header extends React.Component {
             ></img>
             <div id="header-text">League Library</div>
           </div>
-          <div id="header-email">
-            gonda.armand@gmail.com
-            <img
-              src="../../email.webp"
-              alt="email-icon"
-              id="email-icon"
-              className="non-selectable"
-            ></img>
-            {/* <img src = "https://upload.wikimedia.org/wikipedia/commons/7/7e/Gmail_icon_%282020%29.svg" alt = "email-icon" id = "email-icon"></img> */}
-          </div>
-          <div id="header-github">
-            <img
-              src="../../github.webp"
-              alt="github-icon"
-              id="github-icon"
-              className="non-selectable"
-            ></img>
-            <a
-              href="https://github.com/Xenonyy"
-              style={{ position: 'absolute', screenLeft: '4em' }}
-            >
-              Xenonyy
-            </a>
-          </div>
-          <div id="header-dropdown">
-            <button className="dropbtn">▼</button>
-            <div className="dropdown-content">
-              <a href="https://github.com/Xenonyy/league-library">
-                View this specific project's repository.
+          <div id="header-personal-info">
+            <div id="header-email">
+              <img
+                src="../../email.webp"
+                alt="email-icon"
+                id="email-icon"
+                className="non-selectable"
+              ></img>
+              <a
+                href="mailto:gonda.armand@gmail.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="header-personal-link"
+              >
+                gonda.armand@gmail.com
               </a>
+            </div>
+            <div id="header-github">
+              <img
+                src="../../github.webp"
+                alt="github-icon"
+                id="github-icon"
+                className="non-selectable"
+              ></img>
+              <a
+                href="https://github.com/Xenonyy"
+                rel="noopener noreferrer"
+                target="_blank"
+                className="header-personal-link"
+              >
+                Xenonyy
+              </a>
+            </div>
+            <div id="header-dropdown">
+              <button className="dropbtn">▼</button>
+              <div className="dropdown-content">
+                <a
+                  href="https://github.com/Xenonyy/league-library"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  View this specific project's repository.
+                </a>
+              </div>
             </div>
           </div>
         </div>
