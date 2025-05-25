770373a40f4646a5da239ba73c5bf2f575606e8f
Xenonyy <gonda.armand@gmail.com>
2025-05-25 17:32:45 +0200

    fix: css

--- src/components/layout/BackToTop/BackToTop.jsx
+++ src/components/layout/BackToTop/BackToTop.jsx
@@ -21,24 +21,14 @@ function BackToTop() {
     <button
       id="b2t"
       onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
-      style={{
-        display: 'block',
-        position: 'fixed',
-        bottom: '20px',
-        right: '20px',
-        zIndex: 1000,
-        background: 'none',
-        border: 'none',
-        cursor: 'pointer',
-      }}
+      style={{ display: visible ? 'block' : 'none' }}
     >
       <img
         src="https://i.gyazo.com/8fde68144b0fe90786b9472cdcad77f1.png"
         srcSet="https://i.gyazo.com/8fde68144b0fe90786b9472cdcad77f1.png, https://i.gyazo.com/25bca84d684ddfe5d7dd4aaec54f92e6.png"
         alt="Scroll To The Top"
         id="b2t-img"
-        style={{ width: '40px', height: '40px' }}
-      />
+      ></img>
     </button>
   );
 }
