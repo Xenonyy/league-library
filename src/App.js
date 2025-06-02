a27b86b92a55c9cfa0e8902fe22625fb9606bd84
Xenonyy <gonda.armand@gmail.com>
2025-06-02 12:39:22 +0200

    refactor: react19

--- src/App.js
+++ src/App.js
@@ -1,27 +1,50 @@
-import BackToTop from './components/layout/BackToTop/BackToTop';
-import ChampionData from './components/champions/ChampionData';
-import Footer from './components/layout/Footer/Footer';
-import { Results } from './components/search/Results';
-import TopContainer from './components/search/TopContainer';
-import WelcomePage from './components/layout/WelcomePage/WelcomePage';
+import { useState } from 'react';
 import ChampionDetailPanel from './components/champions/ChampionDetailPanel';
+import ChampionMainPage from './components/champions/ChampionMainPage';
+import WelcomePage from './components/layout/WelcomePage/WelcomePage';
 import ConditionalHeader from './components/layout/Header/ConditionalHeader';
+import BackToTop from './components/layout/BackToTop/BackToTop';
+import Footer from './components/layout/Footer/Footer';
+import TopContainer from './components/search/TopContainer';
+import { useChampionData } from './hooks/useFetchChampionData';
 
-function App() {
-  return [
-    <WelcomePage key={'WelcomePage'} />,
-    <div className="container" key={'container'}>
-      <ConditionalHeader />
-      <TopContainer />
-      <div className="champion-container">
-        <ChampionDetailPanel />
-        <ChampionData />
-        <Results />
+const App = () => {
+  const [showWelcome, setShowWelcome] = useState(true);
+  const [selectedChampionId, setSelectedChampionId] = useState(null);
+  const [skinIndex, setSkinIndex] = useState(0);
+  const allData = useChampionData();
+  const [searchTerm, setSearchTerm] = useState('');
+  const handleSearch = value => {
+    setSearchTerm(value);
+  };
+
+  return (
+    <>
+      {showWelcome && <WelcomePage onFinish={() => setShowWelcome(false)} />}
+      <div className="container">
+        <ConditionalHeader />
+        <TopContainer onSearchChange={handleSearch} />
+        <div className="champion-container">
+          {selectedChampionId !== null && (
+            <ChampionDetailPanel
+              champion={allData[selectedChampionId]}
+              skinIndex={skinIndex}
+              setSkinIndex={setSkinIndex}
+              onClose={() => setSelectedChampionId(null)}
+            />
+          )}
+          <ChampionMainPage
+            cards={Object.values(allData)}
+            onChampionClick={setSelectedChampionId}
+            hidden={selectedChampionId !== null}
+            filterText={searchTerm}
+          />
+        </div>
+        <BackToTop />
+        <Footer />
       </div>
-      <BackToTop />
-      <Footer />
-    </div>,
-  ];
-}
+    </>
+  );
+};
 
 export default App;
